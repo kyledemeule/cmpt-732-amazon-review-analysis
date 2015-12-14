d36d1988feced96e9474f0a9c3b4e656b10f2876
Kyle Demeule <kyle.demeule@gmail.com>
2015-12-13 19:38:38 -0800

    Switching name of app.

--- web/application.py
+++ web/application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 from model import Model
-app = Flask(__name__)
+application = Flask(__name__)
 
 REVIEW_PAGE_LENGTH = 10
 
